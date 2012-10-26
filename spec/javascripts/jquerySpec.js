4e75d3fd8b165039eda46e79634a4860f01a5426
Liu Yang <zation1@gmail.com>
2012-10-26 22:47:50 +0800

    should init app

--- spec/javascripts/jquerySpec.js
+++ spec/javascripts/jquerySpec.js
@@ -13,4 +13,20 @@ describe('jquery app', function() {
       '<div id="clear-completed"></div>';
     setFixtures(fixtures);
   });
+
+  it('should initialize app', function() {
+    spyOn(jqueryApp.Utils, 'store').andReturn('some thing');
+    spyOn(jqueryApp, 'cacheElements');
+    spyOn(jqueryApp, 'bindEvents');
+    spyOn(jqueryApp, 'render');
+
+    jqueryApp.init();
+
+    expect(jqueryApp.ENTER_KEY).toEqual(13);
+    expect(jqueryApp.Utils.store).toHaveBeenCalledWith('todos-jquery');
+    expect(jqueryApp.todos).toEqual('some thing');
+    expect(jqueryApp.cacheElements).toHaveBeenCalled();
+    expect(jqueryApp.bindEvents).toHaveBeenCalled();
+    expect(jqueryApp.render).toHaveBeenCalled();
+  });
 });
